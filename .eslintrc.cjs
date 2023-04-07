01e37a8e851fffbe2ebe93b93e6f25e385d555a6
Thomas Cazade <cazade.thomas@gmail.com>
2023-04-07 14:29:21 +0200

    feat: add eslint & prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,17 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-airbnb-with-typescript",
+    "@vue/eslint-config-prettier",
+  ],
+};
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/**/*.vue",
+    "./index.html",
+  ],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+} satisfies Config;

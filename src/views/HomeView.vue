a6057eb7d9a957986758e3f920efdfa796a50644
Thomas Cazade <cazade.thomas@gmail.com>
2023-04-07 14:56:06 +0200

    feat(app): read a segmented stream using hls.js from back-end server

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,31 @@
 <template>
-  <main></main>
+  <main>
+    <!-- eslint-disable-next-line vuejs-accessibility/media-has-caption -->
+    <audio id="audio"></audio>
+
+    <button type="button" @click="handlePlayMusic">Play music</button>
+  </main>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Hls from "hls.js";
+
+const hlsInstance = ref<Hls | null>(null);
+
+const handlePlayMusic = () => {
+  const audio = document.getElementById("audio") as HTMLAudioElement;
+
+  hlsInstance.value = new Hls();
+
+  hlsInstance.value.loadSource(
+    "http://localhost:3003/public/1/320kbps/1-320kbps.m3u8"
+  );
+
+  hlsInstance.value.attachMedia(audio);
+
+  hlsInstance.value.on(Hls.Events.MANIFEST_PARSED, () => {
+    audio.play();
+  });
+};
+</script>

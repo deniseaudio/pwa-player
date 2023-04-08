b99e1fa1d01d19fa927bbe6b4b44765515fb4145
Thomas Cazade <cazade.thomas@gmail.com>
2023-04-08 19:20:01 +0200

    fix: don't use vite pwa service worker

--- vite.config.ts
+++ vite.config.ts
@@ -2,35 +2,9 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { VitePWA } from "vite-plugin-pwa";
-
-import { icons } from "./icons.json";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      srcDir: "src",
-      filename: "service-worker.js",
-      // Compile custom service-worker script and inject the manifest file.
-      strategies: "injectManifest",
-      // Don't automatically register the service worker, we will do it manually.
-      injectRegister: false,
-      manifest: {
-        display: "standalone",
-        lang: "en",
-        id: "?standalone=1",
-        start_url: "/",
-        scope: "/",
-        short_name: "Denise-Audio",
-        name: "Denise-Audio",
-        description: "Denise-Audio application to listen to music.",
-        theme_color: "#1b1917",
-        background_color: "#1b1917",
-        icons,
-      },
-    }),
-  ],
+  plugins: [vue()],
 
   resolve: {
     alias: {

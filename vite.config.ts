858be95c9944674150453347584c8450e9470cb8
Thomas Cazade <cazade.thomas@gmail.com>
2023-04-08 19:08:54 +0200

    feat(pwa): add icons, add manifest properties

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,30 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 
+import { icons } from "./icons.json";
+
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: "autoUpdate",
+      srcDir: "src",
+      filename: "service-worker.js",
+      // Compile custom service-worker script and inject the manifest file.
+      strategies: "injectManifest",
+      // Don't automatically register the service worker, we will do it manually.
+      injectRegister: false,
       manifest: {
-        display: "minimal-ui",
+        display: "standalone",
+        lang: "en",
+        id: "?standalone=1",
+        start_url: "/",
+        scope: "/",
+        short_name: "Denise-Audio",
+        name: "Denise-Audio",
+        description: "Denise-Audio application to listen to music.",
+        theme_color: "#1b1917",
+        background_color: "#1b1917",
+        icons,
       },
     }),
   ],
